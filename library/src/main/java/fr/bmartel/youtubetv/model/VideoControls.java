53a64838edddc537fcbb59e9d334ec8cf651a25c
Bertrand Martel <bmartel.fr@gmail.com>
2016-10-16 20:09:06 +0200

    refactor model + javadoc

---
+++ library/src/main/java/fr/bmartel/youtubetv/model/VideoControls.java
@@ -0,0 +1,57 @@
+/*
+ * The MIT License (MIT)
+ * <p/>
+ * Copyright (c) 2016 Bertrand Martel
+ * <p/>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p/>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p/>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package fr.bmartel.youtubetv.model;
+
+/**
+ * Video controls enum.
+ * Check : https://developers.google.com/youtube/player_parameters?playerVersion=HTML5#controls
+ *
+ * @author  Bertrand Martel
+ */
+public enum VideoControls {
+
+    DEFAULT(1),
+    ALWAYS(2),
+    NONE(0);
+
+    private int mIndex;
+
+    VideoControls(final int index) {
+        this.mIndex = index;
+    }
+
+    public int getIndex() {
+        return mIndex;
+    }
+
+    public static VideoControls getVideoControls(int value) {
+
+        for (VideoControls controls : VideoControls.values()) {
+            if (value == controls.getIndex())
+                return controls;
+        }
+        return DEFAULT;
+    }
+}
