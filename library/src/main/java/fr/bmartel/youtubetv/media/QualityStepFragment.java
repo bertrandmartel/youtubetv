059240a7a6ce06ca02f455ad747e6d6ba673bf54
Bertrand Martel <bmartel.fr@gmail.com>
2016-11-03 17:38:35 +0100

    cleanup : remove unused class/helper, add License header

--- library/src/main/java/fr/bmartel/youtubetv/media/QualityStepFragment.java
+++ library/src/main/java/fr/bmartel/youtubetv/media/QualityStepFragment.java
@@ -1,3 +1,26 @@
+/*
+ * The MIT License (MIT)
+ * <p/>
+ * Copyright (c) 2016 Bertrand Martel
+ * <p/>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p/>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p/>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package fr.bmartel.youtubetv.media;
 
 import android.app.Fragment;
@@ -6,7 +29,6 @@ import android.support.annotation.NonNull;
 import android.support.v17.leanback.app.GuidedStepFragment;
 import android.support.v17.leanback.widget.GuidanceStylist;
 import android.support.v17.leanback.widget.GuidedAction;
-import android.util.Log;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -16,12 +38,10 @@ import fr.bmartel.youtubetv.R;
 import fr.bmartel.youtubetv.model.VideoQuality;
 
 /**
- * The first screen of the rental wizard. Gives the user the choice between renting the movie in SD
- * or HD quality.
+ * Select available video quality step fragment.
  */
 public class QualityStepFragment extends GuidedStepFragment {
 
-    public static final String TAG = "WizardExample1st";
     private final static String QUALITY_LIST_KEY = "qualityList";
 
     private List<VideoQuality> mVideoQualities = new ArrayList<>();
@@ -29,20 +49,13 @@ public class QualityStepFragment extends GuidedStepFragment {
     @NonNull
     @Override
     public GuidanceStylist.Guidance onCreateGuidance(Bundle savedInstanceState) {
-        GuidanceStylist.Guidance guidance = new GuidanceStylist.Guidance("Select a video quality", "", "", null);
+        GuidanceStylist.Guidance guidance = new GuidanceStylist.Guidance(getResources().getString(R.string.video_quality_title), "", "", null);
         return guidance;
     }
 
     @Override
     public int onProvideTheme() {
         return R.style.Theme_Example_LeanbackWizard;
-    }
-
-    VideoExampleActivity getWizardActivity() {
-        if (!(getActivity() instanceof VideoExampleActivity)) {
-            throw new IllegalStateException(VideoExampleActivity.class.getName() + " expected.");
-        }
-        return (VideoExampleActivity) getActivity();
     }
 
     @Override
@@ -55,9 +68,6 @@ public class QualityStepFragment extends GuidedStepFragment {
     public void onCreateActions(@NonNull List<GuidedAction> actions, Bundle savedInstanceState) {
 
         mVideoQualities = (List<VideoQuality>) getArguments().getSerializable(QUALITY_LIST_KEY);
-        for (int i = 0; i < mVideoQualities.size(); i++) {
-            Log.i(TAG, "mVideoQualities : " + i + " : " + mVideoQualities.get(i).getValue());
-        }
 
         for (VideoQuality quality : mVideoQualities) {
             GuidedAction action = new GuidedAction.Builder(getActivity())
@@ -72,9 +82,7 @@ public class QualityStepFragment extends GuidedStepFragment {
 
     @Override
     public void onGuidedActionClicked(GuidedAction action) {
-        //GuidedStepFragment fragment = WizardExample2ndStepFragment.build(rentHd, this);
-        //add(getFragmentManager(), fragment);
-        ((VideoExampleActivity) getActivity()).setVideoQuality(action.getId());
+
     }
 
     public static Fragment newInstance(List<VideoQuality> availableQualities) {
