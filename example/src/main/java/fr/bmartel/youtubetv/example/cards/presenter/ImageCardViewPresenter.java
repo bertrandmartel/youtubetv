dda02d2ffc5b432021aee7ec9118c0de4a232b0d
Bertrand Martel <bmartel.fr@gmail.com>
2016-11-04 19:30:45 +0100

    setup YoutubeTv example module with VericalGrid

---
+++ example/src/main/java/fr/bmartel/youtubetv/example/cards/presenter/ImageCardViewPresenter.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package fr.bmartel.youtubetv.example.cards.presenter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v17.leanback.widget.ImageCardView;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+
+import com.squareup.picasso.Picasso;
+
+import fr.bmartel.youtubetv.example.R;
+import fr.bmartel.youtubetv.example.model.Card;
+import fr.bmartel.youtubetv.example.samples.YoutubeActivityApiShowcase;
+import fr.bmartel.youtubetv.example.samples.YoutubeActivityFragment;
+import fr.bmartel.youtubetv.example.samples.YoutubeTvViewDebug;
+import fr.bmartel.youtubetv.example.samples.YoutubeTvViewFullScreen;
+import fr.bmartel.youtubetv.example.samples.YoutubeTvViewSplitted;
+
+/**
+ * A very basic {@link ImageCardView} {@link android.support.v17.leanback.widget.Presenter}.You can
+ * pass a custom style for the ImageCardView in the constructor. Use the default constructor to
+ * create a Presenter with a default ImageCardView style.
+ */
+public class ImageCardViewPresenter extends AbstractCardPresenter<ImageCardView> {
+
+    private Card.Type mType;
+
+    public ImageCardViewPresenter(Context context, int cardThemeResId) {
+        super(new ContextThemeWrapper(context, cardThemeResId));
+    }
+
+    public ImageCardViewPresenter(Context context, Card.Type type) {
+        this(context, R.style.DefaultCardTheme);
+        mType = type;
+    }
+
+    public ImageCardViewPresenter(Context context) {
+        this(context, R.style.DefaultCardTheme);
+    }
+
+    @Override
+    protected ImageCardView onCreateView() {
+        ImageCardView imageCardView = new ImageCardView(getContext());
+        imageCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent;
+
+                switch (mType) {
+                    case YOUTUBETV_FRAGMENT:
+                        intent = new Intent(getContext(), YoutubeActivityFragment.class);
+                        getContext().startActivity(intent);
+                        break;
+                    case YOUTUBETV_FULLSCREEN:
+                        intent = new Intent(getContext(), YoutubeTvViewFullScreen.class);
+                        getContext().startActivity(intent);
+                        break;
+                    case YOUTUBETV_API:
+                        intent = new Intent(getContext(), YoutubeActivityApiShowcase.class);
+                        getContext().startActivity(intent);
+                        break;
+                    case YOUTUBETV_SPLITTED:
+                        intent = new Intent(getContext(), YoutubeTvViewSplitted.class);
+                        getContext().startActivity(intent);
+                        break;
+                    case YOUTUBETV_DEBUG:
+                        intent = new Intent(getContext(), YoutubeTvViewDebug.class);
+                        getContext().startActivity(intent);
+                        break;
+                }
+            }
+        });
+        return imageCardView;
+    }
+
+    @Override
+    public void onBindViewHolder(Card card, final ImageCardView cardView) {
+        cardView.setTag(card);
+        cardView.setTitleText(card.getTitle());
+        cardView.setContentText(card.getDescription());
+        if (card.getLocalImageResourceName() != null) {
+            int resourceId = getContext().getResources()
+                    .getIdentifier(card.getLocalImageResourceName(),
+                            "drawable", getContext().getPackageName());
+            Picasso.with(getContext()).load(resourceId).into(cardView.getMainImageView());
+        }
+    }
+
+}
---
+++ example/src/main/java/fr/bmartel/youtubetv/example/samples/YoutubeActivityApiShowcase.java
@@ -0,0 +1,108 @@
+package fr.bmartel.youtubetv.example.samples;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import fr.bmartel.youtubetv.YoutubeTvView;
+import fr.bmartel.youtubetv.example.R;
+import fr.bmartel.youtubetv.listener.IPlayerListener;
+import fr.bmartel.youtubetv.model.VideoInfo;
+import fr.bmartel.youtubetv.model.VideoState;
+
+/**
+ * YoutubeActivityApiShowcase
+ *
+ * @author Bertrand Martel
+ */
+public class YoutubeActivityApiShowcase extends Activity {
+
+    private final static String TAG = YoutubeActivityApiShowcase.class.getSimpleName();
+
+    private YoutubeTvView mYoutubeView1;
+
+    private final static int POSITION_OFFSET = 5;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_api_showcase);
+
+        mYoutubeView1 = (YoutubeTvView) findViewById(R.id.video1);
+
+        Button playBtn = (Button) findViewById(R.id.play_button);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mYoutubeView1.start();
+            }
+        });
+        Button pauseBtn = (Button) findViewById(R.id.pause_button);
+        pauseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mYoutubeView1.pause();
+            }
+        });
+        Button nextBtn = (Button) findViewById(R.id.next_button);
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mYoutubeView1.nextVideo();
+            }
+        });
+        Button previousBtn = (Button) findViewById(R.id.previous_button);
+        previousBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mYoutubeView1.previousVideo();
+            }
+        });
+
+        Button backwardBtn = (Button) findViewById(R.id.backward_button);
+        backwardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mYoutubeView1.moveBackward(POSITION_OFFSET);
+            }
+        });
+        Button forwardBtn = (Button) findViewById(R.id.forward_button);
+        forwardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mYoutubeView1.moveForward(POSITION_OFFSET);
+            }
+        });
+
+        mYoutubeView1.addPlayerListener(new IPlayerListener() {
+            @Override
+            public void onPlayerReady(final VideoInfo videoInfo) {
+                Log.i(TAG, "onPlayerReady");
+            }
+
+            @Override
+            public void onPlayerStateChange(final VideoState state,
+                                            final long position,
+                                            final float speed,
+                                            final float duration,
+                                            final VideoInfo videoInfo) {
+                Log.i(TAG, "onPlayerStateChange : " + state.toString() + " | position : " + position + " | speed : " + speed);
+            }
+        });
+        requestVisibleBehind(true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        requestVisibleBehind(true);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+}
